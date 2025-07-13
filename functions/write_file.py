5f5b93c86ee86fa52e9ba66c823f4e0eaadf407d
Xaitan <utrustad.sprang_0z@icloud.com>
2025-07-13 22:56:09 +0200

    I can write

--- functions/write_file.py
+++ functions/write_file.py
@@ -8,6 +8,11 @@ def write_file(working_directory, file_path, content):
         if os.path.commonpath([working_directory_abs, file_path_abs]) != working_directory_abs:
             return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
 
-    
+        directory = os.path.dirname(file_path_abs)
+        os.makedirs(directory, exist_ok=True)
+
+        with open(file_path_abs, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
         return (f"Error:  {e}")
--- tests.py
+++ tests.py
@@ -1,14 +1,14 @@
 #from functions.get_files_info import get_files_info
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 def run_tests():
     print("Result for current directory:")
-    print(get_file_content("calculator", "main.py"))
+    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
 
     print("\nResult for 'pkg' directory:")
-    print(get_file_content("calculator", "pkg/calculator.py"))
+    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
 
     print("\nResult for '/bin' directory:")
-    print(get_file_content("calculator", "/bin/cat"))
+    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
 
 
 if __name__ == "__main__":
