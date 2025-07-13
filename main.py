0a52f5fa9ae93ac7de1020e1de1d7a81c1659751
Xaitan <utrustad.sprang_0z@icloud.com>
2025-07-13 14:49:54 +0200

    got token count

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import sys
+
+# Load .env variables
+load_dotenv()
+
+# Get the API key from environment
+api_key = os.environ.get("GEMINI_API_KEY")
+
+# Now you can use the key
+client = genai.Client(api_key=api_key)
+
+
+
 def main():
-    print("Hello from ai-agent!")
+    response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+)
+    print(response.text)
+    
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
 
 
 if __name__ == "__main__":
