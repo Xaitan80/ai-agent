fffaa71c32750edd57b3f885d44b4aab6bc9d09c
Xaitan <utrustad.sprang_0z@icloud.com>
2025-07-14 20:31:13 +0200

    one more lesson done

--- functions/run_python.py
+++ functions/run_python.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 def run_python_file(working_directory, file_path):
     try:
@@ -6,5 +7,33 @@ def run_python_file(working_directory, file_path):
         file_path_abs = os.path.abspath(os.path.join(working_directory, file_path))
         if os.path.commonpath([working_directory_abs, file_path_abs]) != working_directory_abs:
             return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        if not os.path.exists(file_path_abs):
+            return f'Error: File "{file_path}" not found.'
+        if not file_path_abs.endswith(".py"):
+            return f'Error: "{file_path}" is not a Python file.'
+        
+        result = subprocess.run(
+            ["python", file_path_abs],
+            capture_output=True,
+            text=True,
+            timeout=30,
+            cwd=working_directory_abs
+        )
+
+        output_parts = []
+
+        if result.stdout:
+            output_parts.append(f"STDOUT:\n{result.stdout.strip()}")
+        if result.stderr:
+            output_parts.append(f"STDERR:\n{result.stderr.strip()}")
+        if result.returncode != 0:
+            output_parts.append(f"Process exited with code {result.returncode}")
+        if not output_parts:
+            return "No output produced."
+
+        return "\n\n".join(output_parts)
+      
+
+
     except Exception as e:
-        return (f"Error:  {e}")
+        f"Error: executing Python file: {e}"
--- tests.py
+++ tests.py
@@ -1,14 +1,15 @@
 #from functions.get_files_info import get_files_info
-from functions.write_file import write_file
+from functions.run_python import run_python_file
 def run_tests():
     print("Result for current directory:")
-    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+    print(run_python_file("calculator", "main.py"))
 
     print("\nResult for 'pkg' directory:")
-    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    print(run_python_file("calculator", "tests.py"))
 
     print("\nResult for '/bin' directory:")
-    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+    print(run_python_file("calculator", "../main.py"))
+    print(run_python_file("calculator", "nonexistent.py"))
 
 
 if __name__ == "__main__":
