59d7c243f80528654826b7f6d9be89598786ba01
Xaitan <utrustad.sprang_0z@icloud.com>
2025-07-16 17:54:23 +0200

    I have the client working

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,7 +1,32 @@
 import os
+from google.genai import types
+#from main import system_prompt
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+'''available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+config=types.GenerateContentConfig(
+    tools=[available_functions], system_instruction=system_prompt
+)'''
 
 def get_files_info(working_directory, directory=None):
     try:
+
         # Resolve absolute paths
         working_directory = os.path.abspath(working_directory)
         directory = os.path.join(working_directory, directory) if directory else working_directory
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import sys
+from functions.get_files_info import schema_get_files_info
 
 # Load .env variables
 load_dotenv()
@@ -26,13 +27,33 @@ def main():
     if len(sys.argv) < 2:
         print("No input provided.")
         sys.exit(1)
-    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
+    available_functions = types.Tool(
+    function_declarations=[schema_get_files_info]
+    )
+    config = types.GenerateContentConfig(
+    tools=[available_functions], system_instruction=system_prompt
+    )
     user_input = sys.argv[1]
+    
     response = client.models.generate_content(
     model='gemini-2.0-flash-001',
     contents=user_input,
-    config=types.GenerateContentConfig(system_instruction=system_prompt),
+    config=config,
 )
+    if response.function_calls:
+        for r in response.function_calls:
+            print (f"Calling function: {r.name}({r.args})")
+   # else:
+       # print(response.text)
     if verbose == True:
     
         print(f"User prompt: {user_input}")
